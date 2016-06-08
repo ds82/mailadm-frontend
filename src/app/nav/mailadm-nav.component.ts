91e3c11c9c447020b58894223473acb3bea98b9c
Dennis Sänger <mail@dennis.io>
2016-06-08 09:13:35 +0200

    feat(nav): add mailadm-nav component

---
+++ src/app/nav/mailadm-nav.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'mailadm-nav',
+    templateUrl: 'mailadm-nav.component.html'
+})
+export class MailadmNavComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
